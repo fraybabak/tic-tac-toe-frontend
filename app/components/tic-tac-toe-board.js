ecd43a5888f6c703c8d9d51b811b6f1d22023949
babak <babak@peachscore.com>
2023-05-21 12:10:15 +0300

    feat: change index.hbs

--- app/components/tic-tac-toe-board.js
+++ app/components/tic-tac-toe-board.js
@@ -3,18 +3,33 @@ import { tracked } from '@glimmer/tracking';
 import { action } from '@ember/object';
 import { set } from '@ember/object';
 export default class TicTacToeBoardComponent extends Component {
-  @tracked board = [
+  @tracked gameBoard = [
     ['', '', ''],
     ['', '', ''],
     ['', '', ''],
   ];
-
+  @tracked currentPlayer = 'X';
   @action
-  onClickCell(row, column) {
-    // Handle cell click event here
-    console.log(`Cell (${row}, ${column}) clicked!`);
-    set(this.board[row], column, 'X'); // Replace "X" with "O" for alternate turns
-    if (this.board[row][column] === '') {
+  async makeMove(row, column) {
+    if (this.gameBoard[row][column] !== '') {
+      return; // Cell already taken
+    }
+
+    let moveResponse = await fetch('/api/moves', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        game_id: this.args.gameId,
+        player_id: this.args.currentPlayerId,
+        move: { row, column },
+      }),
+    });
+
+    if (moveResponse.ok) {
+      this.gameBoard[row][column] = this.currentPlayer;
+      this.currentPlayer = this.currentPlayer === 'X' ? 'O' : 'X';
     }
   }
 }
--- app/controllers/new-game.js
+++ app/controllers/new-game.js
@@ -1,3 +1,70 @@
 import Controller from '@ember/controller';
+import { action } from '@ember/object';
+import { tracked } from '@glimmer/tracking';
 
-export default class NewGameController extends Controller {}
+export default class NewGameController extends Controller {
+  @tracked playerOne = '';
+  @tracked playerTwo = '';
+  @tracked gameId;
+  @tracked playerOneId;
+  @tracked playerTwoId;
+  @tracked currentPlayerId;
+  @tracked p_1;
+  @tracked p_2;
+  @tracked showBoard = false;
+
+  @action
+  updatePlayerOne(event) {
+    this.playerOne = event.target.value;
+  }
+
+  @action
+  updatePlayerTwo(event) {
+    this.playerTwo = event.target.value;
+  }
+
+  @action
+  async startNewGame() {
+    console.log(this.playerOne, this.playerTwo);
+    let playerOne = await createPlayer(this.playerOne);
+    let playerTwo = await createPlayer(this.playerTwo);
+
+    let gameResponse = await fetch('http://localhost:3000/game/create', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        player_one: playerOne.id,
+        player_two: playerTwo.id,
+        game_type: 2,
+      }),
+    });
+
+    if (gameResponse.ok) {
+      let game = await gameResponse.json();
+      this.gameId = game.id;
+      this.playerOneId = game.player_one_id;
+      this.playerTwoId = game.player_two_id;
+      this.currentPlayerId = this.playerOneId; // Assuming player one always starts
+      this.showBoard = true;
+    }
+  }
+}
+
+async function createPlayer(name) {
+  console.log(name, 'sadasdasd');
+  let playerResponse = await fetch('http://localhost:3000/player/create', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      name: name,
+    }),
+  });
+
+  if (playerResponse.ok) {
+    return playerResponse.json();
+  }
+}
